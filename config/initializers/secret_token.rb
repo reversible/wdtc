cdc4a6a29cbca3c65f530446f27c01f95084bce1
Ivan Garcia <raul.ivan.82@gmail.com>
2013-01-26 10:04:39 -0800

    First formal commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdtc::Application.config.secret_token = '4cc602dc545c07fec35704d866029b9e38bd26c96753757185e9cbd63e4c7fa8d599bc1a988df360ca857668976218027c05749b612c46a6f941cceb68e5ca16'
